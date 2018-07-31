057395605f6b9657dead8fa76c418c80a1b84ff4
SumeetTripathi <41904462+SumeetTripathi@users.noreply.github.com>
2018-07-31 14:50:21 +0530

    Add files via upload

--- url-shortening/src/main/java/com/controller/UrlService.java
+++ url-shortening/src/main/java/com/controller/UrlService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cache.RedisCache;
+import com.utils.JedisConnectionManager;
 
 @Controller
 public class UrlService {
@@ -19,34 +20,39 @@ public class UrlService {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-
+		model.addAttribute("serverName", JedisConnectionManager.getServerName());
 		return "home";
 	}
 
 	@RequestMapping(value = "/shrinkurl", method = RequestMethod.POST)
 	public String shrinkUrl(String url, String customUrl, Integer expiry,
-			Integer privateCount, Model model) {
+			String password, Model model) {
+		
+		String key;
+		model.addAttribute("serverName", JedisConnectionManager.getServerName());
+
 		if (customUrl != null && customUrl.trim().length() > 0) {
 			if (redisCache.validateUrl(customUrl)) {
-				model.addAttribute("shrinkurl",
-						redisCache.addUrl(customUrl, url, expiry));
+				key = redisCache.addUrl(customUrl, url, expiry);
+
 			} else {
-				model.addAttribute("shrinkurl",
-						redisCache.addUrl(null, url, expiry));
+				key = redisCache.addUrl(null, url, expiry);
 				model.addAttribute("message",
-						"Custom alias already in use new key is generated");
+						"Custom alias already in use, a new key is generated");
 			}
 		} else {
-			model.addAttribute("shrinkurl",
-					redisCache.addUrl(customUrl, url, expiry));
+			key = redisCache.addUrl(customUrl, url, expiry);
+		}
+		model.addAttribute("shrinkurl", key);
+		if(password!=null && password.trim().length()>0){
+			redisCache.passwordProtected(key, password, expiry);
 		}
 		return "shrinkurl";
 	}
 
 	@RequestMapping(value = "/trackusage", method = RequestMethod.POST)
-	public String get(String shrinkurl, Model model) {
-		System.out.println("shrinkurl" + shrinkurl);
-
+	public String trackUsage(String shrinkurl, Model model) {
+		model.addAttribute("serverName", JedisConnectionManager.getServerName());
 		try {
 			model.addAttribute("hits", redisCache.getHits(shrinkurl));
 		} catch (Throwable e) {
@@ -58,12 +64,32 @@ public class UrlService {
 	}
 
 	@RequestMapping(value = "/uri/{url}", method = RequestMethod.GET)
-	public String geturl(@PathVariable("url") String url, Model model) {
-		System.out.println("url" + url);
-
+	public String getUrl(@PathVariable("url") String url, Model model) {
+		model.addAttribute("serverName", JedisConnectionManager.getServerName());
 		try {
-			model.addAttribute("shrinkurl", redisCache.getUrl(url));
-			return "geturl";
+			if(redisCache.isPasswordProtected(url)){
+				model.addAttribute("serverName", JedisConnectionManager.getServerName());
+				model.addAttribute("shrinkurl", url);
+				return "pwdprotected";
+			}else{
+				model.addAttribute("shrinkurl", redisCache.getUrl(url));
+				return "geturl";
+			}
+		} catch (Exception e) {
+			return "notfound";
+		}
+	}
+	
+	@RequestMapping(value = "uri/pwdprotected", method = RequestMethod.POST)
+	public String getPwdProtectedUrl(String url,String pwd, Model model) {
+		model.addAttribute("serverName", JedisConnectionManager.getServerName());
+		try {
+			if(redisCache.isPasswordCorrect(url, pwd)){
+				model.addAttribute("shrinkurl", redisCache.getUrl(url));
+				return "geturl";
+			}else{
+				return "notfound";
+			}
 		} catch (Exception e) {
 			return "notfound";
 		}
