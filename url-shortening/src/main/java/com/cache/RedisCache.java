bc7452fe2c97f7a25674763513e5a0566227463f
SumeetTripathi <41904462+SumeetTripathi@users.noreply.github.com>
2018-07-31 14:49:37 +0530

    Add files via upload

--- url-shortening/src/main/java/com/cache/RedisCache.java
+++ url-shortening/src/main/java/com/cache/RedisCache.java
@@ -14,4 +14,10 @@ public interface RedisCache {
 
 	public String getHits(String hash) throws Throwable;
 
+	public void passwordProtected(String key, String password, Integer minutes);
+
+	public Boolean isPasswordProtected(String key);
+
+	public Boolean isPasswordCorrect(String key, String pwd);
+
 }
